2187195f1acb6a3853b031ba9ef9a306ed02b9e9
Mézes Ádám <mezesadam1997@gmail.com>
2022-04-23 16:10:43 +0200

    changed KeyPressHandler interface and class to use the focus handler fixed direction change now works correct

--- src/handlers/KeyPressHandler.ts
+++ src/handlers/KeyPressHandler.ts
@@ -1,11 +1,16 @@
+import IFocusHandler from '../interfaces/IFocusHandler';
 import IKeyPressHandler from '../interfaces/IKeyPressHandler';
 import TDirection from '../types/TDirection';
+import THotkey from '../types/THotkey';
+import FocusHandler from './FocusHandler';
 
 class KeyPressHandler implements IKeyPressHandler {
   direction: TDirection;
+  focusHandler: IFocusHandler;
 
   constructor() {
-    this.direction = 1;
+    this.direction = 1,
+    this.focusHandler = new FocusHandler();
 
     document.addEventListener('keydown', this.handleKeyDown.bind(this));
   }
@@ -16,12 +21,20 @@ class KeyPressHandler implements IKeyPressHandler {
    */
   handleKeyDown(e: KeyboardEvent): void {
     switch (e.code) {
+      // Moves focus
+      case 'KeyH':
+      case 'KeyL':
+      case 'KeyM':
+        const hotkey = e.code.replace(/key/i, '').toUpperCase() as THotkey;
+        this.focusHandler.move(hotkey, this.direction);
+        break;
+      
       // Changes the direction of the focus movement
       case 'ArrowUp':
-        this.setDirection(1);
+        this.setDirection(-1);
         break;
       case 'ArrowDown':
-        this.setDirection(-1);
+        this.setDirection(1);
         break;
     }
   }
--- src/interfaces/IKeyPressHandler.ts
+++ src/interfaces/IKeyPressHandler.ts
@@ -1,7 +1,9 @@
+import IFocusHandler from './IFocusHandler';
 import TDirection from '../types/TDirection';
 
 interface IKeyPressHandler {
   direction: TDirection,
+  focusHandler: IFocusHandler,
 
   constructor: Function,
   handleKeyDown: (e: KeyboardEvent) => void,
